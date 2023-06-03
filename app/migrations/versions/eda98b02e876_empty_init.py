4f94fd75b638927c91a7409176383133ab7f5201
titus <titus.unavailable@student.moringaschool.com>
2023-06-04 00:06:24 +0300

    Assignment complete

---
+++ app/migrations/versions/eda98b02e876_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: eda98b02e876
+Revises: 
+Create Date: 2023-06-03 23:37:08.055563
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eda98b02e876'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
